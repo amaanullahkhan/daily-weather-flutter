fac8f4e46364036ec5f9cc33f4ff51407f300d98
Amaan Ullah <amaan.ullah@venturedive.com>
2019-12-18 11:59:24 +0500

    gitignore file upated. duplicate file removed. string_constants file created.

---
+++ project/weather/lib/constants/string_constants.dart
@@ -0,0 +1,6 @@
+class StringConstants {
+  
+  static StringConstants standard = StringConstants();
+
+  String get openWeatherApiKey => "ae0027368366977769d812a219b6e19e";
+}
--- project/weather/lib/main.dart
+++ project/weather/lib/main.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:weather/weather.dart';
+import 'package:weather_app/constants/string_constants.dart';
 import 'home_page/home_page.dart';
 
-
 void main() async {
-    runApp(MyApp());
-} 
-
+  WidgetsFlutterBinding.ensureInitialized();
+  var weatherStation = WeatherStation(StringConstants.standard.openWeatherApiKey);
+  var weather = await weatherStation.currentWeather();
+  print(weather.toString());
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
